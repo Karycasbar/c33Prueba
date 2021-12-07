ffcbed67447549a65b5939436172947c77b48524
Karycasbar <karycasbar@gmail.com>
2021-12-07 12:01:58 -0600

    se agrego todo lo de la resortera

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg;
 var platform;
-var log6, chain;
+var log6, slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -39,8 +39,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80,PI/2);
-    chain = new Chain(bird.body,log6.body);
+    //log6 = new Log(230,180,80,PI/2);
+    slingshot = new Slingshot(bird.body,{x:200,y:100});
 
     
     
@@ -71,8 +71,14 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();
+    //log6.display();
+    slingshot.display();
 
     
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY});
+}
+function mouseReleased(){
+    slingshot.fly();
 }
