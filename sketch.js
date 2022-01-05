d503165df7a690e085f6fd23c6d88faa52b5e291
Karycasbar <karycasbar@gmail.com>
2022-01-05 13:48:57 -0600

    funcion keyPressed

--- sketch.js
+++ sketch.js
@@ -51,9 +51,9 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
+    /*console.log(box2.body.position.x);
     console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    console.log(box2.body.angle);*/
     box1.display();
     box2.display();
     ground.display();
@@ -81,4 +81,9 @@ function mouseDragged(){
 }
 function mouseReleased(){
     slingshot.fly();
+}
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
 }
