b6587aca66035b7a0510e868d5ec3d35d5d8525e
Karycasbar <karycasbar@gmail.com>
2022-01-08 12:51:40 -0600

    Desvanecer enemigo

--- Pig.js
+++ Pig.js
@@ -5,7 +5,7 @@ class Pig extends BaseClass {
     this.Visiblity = 255;
   }
   display(){
-    console.log(this.body.speed);
+    //console.log(this.body.speed);
     if(this.body.speed<3){
       super.display();
     }else{
@@ -15,7 +15,13 @@ class Pig extends BaseClass {
       tint(255,this.Visiblity);
       image(this.image, this.body.position.x,this.body.position.y,50,50);
       pop();
-    }
-    
+    }    
   }
+
+  score(){
+    if(this.Visiblity <0 && this.Visiblity >-1005){
+      score++;
+    }
+  }
+
 }
